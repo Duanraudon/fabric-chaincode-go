42957f1938d0a7dc6c5d88bbb8d197411620ef4f
AC <guobo.cao@reddatetech.com>
2026-04-30 14:11:26 +0800

    FABRIC 2.5.2依赖库FABRIC-CHAINCODE-GO代码添加

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/hyperledger/fabric-chaincode-go
+
+go 1.17
+
+require (
+	github.com/golang/protobuf v1.5.2
+	github.com/hyperledger/fabric-protos-go v0.0.0-20220613214546-bf864f01d75e
+	github.com/stretchr/testify v1.8.0
+	github.com/tjfoc/gmsm v1.4.1
+	github.com/tjfoc/gmtls v1.2.1
+	google.golang.org/grpc v1.48.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20201012173705-84dcc777aaee // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
+	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+replace github.com/tjfoc/gmtls v1.2.1 => gitea.com/tjfoc/gmtls v1.2.1
+replace github.com/tjfoc/gmsm v1.4.1 => gitea.com/tjfoc/gmsm v1.1.1
---
+++ utils/ca.go
@@ -0,0 +1,67 @@
+package utils
+
+import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/ed25519"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"github.com/tjfoc/gmsm/sm2"
+	"strings"
+)
+
+func ParseSM2PrivateKey(pemBytes []byte) (*sm2.PrivateKey, bool) {
+	if len(pemBytes) == 0 {
+		return nil, false
+	}
+	block, pemBytes := pem.Decode(pemBytes)
+	if block == nil {
+		return nil, false
+	}
+	if block.Type == "PRIVATE KEY" || strings.HasSuffix(block.Type, " PRIVATE KEY") {
+		if priKey, err := parsePrivateKey(block.Bytes); err == nil {
+			if priKey, ok := priKey.(*sm2.PrivateKey); ok {
+				return priKey, true
+			}
+		}
+	}
+	return nil, false
+}
+
+func ParseECDSAPrivateKey(pemBytes []byte) (*ecdsa.PrivateKey, bool) {
+	if len(pemBytes) == 0 {
+		return nil, false
+	}
+	block, pemBytes := pem.Decode(pemBytes)
+	if block == nil {
+		return nil, false
+	}
+	if block.Type == "PRIVATE KEY" || strings.HasSuffix(block.Type, " PRIVATE KEY") {
+		if priKey, err := parsePrivateKey(block.Bytes); err == nil {
+			if priKey, ok := priKey.(*ecdsa.PrivateKey); ok {
+				return priKey, true
+			}
+		}
+	}
+	return nil, false
+}
+
+func parsePrivateKey(der []byte) (crypto.PrivateKey, error) {
+	if key, err := x509.ParsePKCS1PrivateKey(der); err == nil {
+		return key, nil
+	}
+	if key, err := x509.ParsePKCS8PrivateKey(der); err == nil {
+		switch key := key.(type) {
+		case *rsa.PrivateKey, *ecdsa.PrivateKey, ed25519.PrivateKey:
+			return key, nil
+		default:
+			return nil, errors.New("tls: found unknown private key type in PKCS#8 wrapping")
+		}
+	}
+	if key, err := sm2.ParsePKCS8UnecryptedPrivateKey(der); err == nil {
+		return key, nil
+	}
+	return nil, errors.New("tls: failed to parse private key")
+}
